c3125f4dd5d1e7ea3e68309f83c603da995bcc6f
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-04 13:02:55 +0200

    add perlin module

--- landscape.hpp
+++ landscape.hpp
@@ -1,4 +1,13 @@
 #ifndef LANDSCAPE_H
 #define LANDSCAPE_H
 
+#include <libnoise/noise.h>
+#include "parameters.hpp"
+
+using namespace noise;
+
+/// set rng seed using GSL
+
+/// make landscape as perlin module
+module::Perlin landscape;
 #endif // LANDSCAPE_H
