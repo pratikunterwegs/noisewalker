69346e8f6d06e8d6a62f53f797610b926972c351
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-04 14:13:17 +0200

    add main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <libnoise/noise.h>
-
-using namespace noise;
+#include "parameters.hpp"
+#include "landscape.hpp"
+#include "agent.hpp"
+#include "simulation.hpp"
 
 int main(int argc, char *argv[])
 {
+    std::vector<std::string> cliArgs(argv, argv+argc);
+
+    // init gsl rng
+    const gsl_rng_type * T;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc (T);
+
+    unsigned seed = static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count());
+    gsl_rng_set(r, seed);
+
+    do_simulation(cliArgs);
 
     return 0;
 }
