d24a398efa077096a0394140aaaf8d32fdf05fc8
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-04 13:01:58 +0200

    add basic parameters

--- parameters.hpp
+++ parameters.hpp
@@ -1,4 +1,27 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
+#include <random>
+// gsl libs
+#include <stdio.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+
+// population params
+const int popSize = 500; // not too many
+const int genmax = 1000; // a few generations
+
+// landscape params
+const float landsize = 100.f;
+
+// mutation params
+const float mShift = 0.01f;
+const float mProb = 0.01f;
+const float mShiftA = 0.05f;
+
+// make a gsl rng and a regular rng
+gsl_rng * r ;
+std::mt19937 rng;
+
 #endif // PARAMETERS_H
