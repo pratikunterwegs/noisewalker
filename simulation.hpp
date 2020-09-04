9a9243b75a3187fa07dae3a873b1a2a82650748e
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-04 15:42:36 +0200

    edits to sim

--- simulation.hpp
+++ simulation.hpp
@@ -2,26 +2,27 @@
 #define SIMULATION_H
 
 #include <iostream>
+#include <libnoise/noise.h>
+#include "noiseutils.h"
 #include <parameters.hpp>
-#include <landscape.hpp>
 #include <agent.hpp>
 
 /// function to evolve population
-/// THIS WHOLE THING NEEDS TO CHANGE
 void evolvePop(std::vector<Agent> &pop,
                const int genmax, const int timesteps,
+               const double tempRate,
                module::Perlin landscape)
 {
     auto t1 = std::chrono::high_resolution_clock::now();
-    // loop through timesteps
-        for (int t = 0; t < maxTimesteps; ++t) {
-            if (t % timePerGen == 0) {
-                // reproduce
-                doReproduce(pop);
-            }
+    double coordZ = 0.0;
+    for (int gen = 0; gen < genmax; ++gen) {
+        for (int t = 0; t < timesteps; ++t) {
             // if gen has not changed then move and forage
-            popMoveForage(pop, t, landscape);
+            popMoveForage(pop, coordZ, landscape);
+            coordZ += tempRate;
         }
+        doReproduce(pop);
+    }
     auto t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
     std::cout << "pop evolve time: " << duration << "\n";
@@ -36,20 +37,24 @@ void do_simulation(std::vector<std::string> cliArgs){
     const int timesteps = std::stoi(cliArgs[2]);
     const int nOctaves = std::stoi(cliArgs[3]);
     const double frequency = std::stod(cliArgs[4]);
-    const int newSrange = std::stof(cliArgs[5]);
-    std::string rep = cliArgs[6];
+    const double tempRate = std::stod(cliArgs[5]);
+    const int newSrange = std::stof(cliArgs[6]);
+    std::string rep = cliArgs[7];
 
     // init pop & landscape
     std::vector<Agent> pop (popSize);
-    module::Perlin landscape = makeNoise(nOctaves, frequency);
-    // force sensory range
+    module::Perlin noise;
+    noise.SetOctaveCount(nOctaves);
+    noise.SetFrequency(frequency);
+    noise.SetPersistence(0.5);
+
     forceSrange(pop, newSrange);
 
     // print outpath for test
     // std::cout << "data at " << thisOutpath[0] << thisOutpath[1] << "\n";
 
     // do evolution
-    evolvePop(pop, genmax, timesteps, landscape);
+    evolvePop(pop, genmax, timesteps, tempRate, noise);
     
     std::cout << "pop evolved!" << "\n";
 
