d335ad78a971a4c742d15d05dd38d6c0ba17932e
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-07 23:06:57 +0200

    minor change

--- R/analysis.R
+++ R/analysis.R
@@ -9,7 +9,7 @@ lookup <- fread("data/lookup.csv")
 lookup <- split(lookup, lookup$filename)
 
 # read in
-data <- lapply(files[4], fread)
+data <- lapply(files, fread)
 
 # link to file
 data <- mapply(function(df, params) {
@@ -18,11 +18,14 @@ data <- mapply(function(df, params) {
 }, data, lookup, SIMPLIFY = FALSE)
 
 # rbind list
-data <- rbindlist(data)
+data_plot <- rbindlist(data[4])
 
 # plot
-ggplot(data[1:1e4,])+
-  geom_line(aes(cue, distance,
-                group = id), alpha = 0.1)+
+ggplot(data_plot)+
+  geom_vline(xintercept = 0, col = "red", lty = 2)+
+  geom_line(aes(cue, distance + 0.1,
+                group = id), 
+            colour = "grey20",
+            alpha = 0.1)+
   facet_grid(tempRate ~ frequency, 
              labeller = label_both)
