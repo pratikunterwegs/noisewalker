7383bea5174be1f4f38b7d0fb2cfd11c32c873a5
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-08 22:02:29 +0200

    add prelim 2d sim

--- R/analysis.R
+++ R/analysis.R
@@ -18,14 +18,21 @@ data <- mapply(function(df, params) {
 }, data, lookup, SIMPLIFY = FALSE)
 
 # rbind list
-data_plot <- rbindlist(data[4])
+data_plot <- rbindlist(data)
 
+x11()
 # plot
-ggplot(data_plot)+
-  geom_vline(xintercept = 0, col = "red", lty = 2)+
+ggplot(data_plot[id %% 1 ==0,])+
+  geom_vline(xintercept = 0, col = "red", lty = 2,
+             size = 0.1)+
   geom_line(aes(cue, distance + 0.1,
-                group = id), 
-            colour = "grey20",
-            alpha = 0.1)+
+                group = id,
+                colour = ifelse(distance < 0.01, "non-move", "move")),
+            alpha = 0.1,
+            show.legend = F)+
   facet_grid(tempRate ~ frequency, 
-             labeller = label_both)
+             labeller = label_both)+
+  theme_void()+
+  coord_cartesian(ylim=c(0.1, 10),
+                  xlim=c(-0.5, 0.5))
+ggsave(filename = "figures/fig_rnorm_08_sep_2020.png", dpi = 300)
