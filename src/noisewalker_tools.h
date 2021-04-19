19227d38cbe5dae93ed25abf48b5a94c9b2d3d4e
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-19 14:09:10 +0200

    add tools

--- src/noisewalker_tools.h
+++ src/noisewalker_tools.h
@@ -8,5 +8,40 @@
 #include <cmath>
 #include <algorithm> 
 #include <cassert>
+#include <Rcpp.h>
+
+using namespace Rcpp;
+struct genData {
+public:
+    std::vector<std::vector<int> > gen;
+    std::vector<std::vector<int> > strategy;
+    std::vector<std::vector<double> > prop;
+
+    // fun
+    void updateGenData (std::vector<Agent> &pop, const int gen);
+    Rcpp::List returnGenData ();
+};
+
+void genData::updateGenData (std::vector<Agent> &pop, const int g) {
+
+    std::vector<double> strategyProp = getPopStrategyProp(pop);
+    std::vector<int> stratvec = {0, 1, 2};
+
+    gen.push_back(std::vector<int> {g, g, g});
+    strategy.push_back(stratvec);
+    prop.push_back(strategyProp);
+}
+
+Rcpp::List genData::returnGenData() {
+    Rcpp::List genDataList (gen.size());
+    for(size_t li = 0; li < gen.size(); li++) {
+        genDataList[li] = Rcpp::DataFrame::create(
+                    Named("gen") = gen[li],
+                    Named("strategy") = strategy[li],
+                    Named("prop") = prop[li]);
+    }
+
+    return genDataList;
+}
 
 #endif // NOISEWALKER_TOOLS_H
