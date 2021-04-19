a6941bc904378312a1bd8e2c19753cfaa82247ec
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-19 20:21:46 +0200

    add move cost

--- src/parameters.h
+++ src/parameters.h
@@ -15,19 +15,15 @@ const int timePerGen = 100;
 const int maxTimesteps = timePerGen * generations; // a few generations
 
 // movement cost
-// const float move_cost = 0.001f;
-const float mass_init = 10.f;
-const float mass_min = 0.001f;
-const double mass_move_ratio = 1.f;
-const float mass_cost = 0.001f;
+const float move_cost = 0.01f;
 
 // landscape params
 const float landsize = 100.f;
-const float clamp = 0.5f;
+const float clamp = 0.0f;
 
 // mutation params
 const float mShift = 0.01f;
-const float mProb = 0.01f;
+const float mProb = 0.001f;
 const float mShiftA = 0.05f;
 
 // make a gsl rng and a regular rng
