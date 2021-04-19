0a0da4f0190b61059660387ac80219ec1ab2df47
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-19 15:37:31 +0200

    add main for qtcr run check

--- noisewalker_cpp/main.cpp
+++ noisewalker_cpp/main.cpp
@@ -1,8 +1,36 @@
 #include <QCoreApplication>
+#include "../src/parameters.h"
+#include "../src/FastNoiseLite.h"
+#include "../src/agent.h"
+#include "../src/noisewalker_tools.h"
+#include "../src/simulation.cpp"
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    // set the random number generation etc
+    const gsl_rng_type * T;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc (T);
 
-    return a.exec();
+    unsigned seed = static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count());
+
+    // init pop
+    std::vector<Agent> pop (50, Agent(1, 1.f));
+    // random position
+    popRandomPos(pop, landsize);
+    // random weights
+    popRandomWeights(pop);
+
+    // make the ancestral landscape
+    FastNoiseLite noise;
+    noise.SetSeed(seed);
+    noise.SetFrequency(2.0);
+    noise.SetFractalOctaves(2);
+
+    // do evolution
+    Rcpp::List thisData = evolvePop(pop, 50, 10, 0.01, noise, landsize);
+
+    std::cout << "pop evolved";
+    return 0;
 }
