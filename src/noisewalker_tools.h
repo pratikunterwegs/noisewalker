0efa8fcb9bcbf8f7d07eca948300322e6a872c1a
Pratik Gupte <pratikgupte16@gmail.com>
2021-04-27 14:44:36 +0200

    move data

--- src/noisewalker_tools.h
+++ src/noisewalker_tools.h
@@ -16,7 +16,6 @@ public:
     std::vector<std::vector<int> > gen;
     std::vector<std::vector<int> > strategy;
     std::vector<std::vector<double> > prop;
-    std::vector<std::vector<double> > dist_moved;
     void updateGenData (std::vector<Agent> &pop, const int gen);
     Rcpp::List returnGenData ();
 };
@@ -28,6 +27,30 @@ public:
     void updateFitnessData(std::vector<Agent> &pop, const int gen);
     Rcpp::List returnFitnessData ();
 };
+
+struct moveData {
+public:
+    std::vector<double> moved;
+    std::vector<int> strategies;
+
+    void updateMoveData(std::vector<Agent> &pop);
+    Rcpp::DataFrame returnMoveData();
+};
+
+void moveData::updateMoveData (std::vector<Agent> &pop) {
+    for (size_t i = 0; i < pop.size(); i++) {
+        moved.push_back(pop[i].moved);
+        strategies.push_back(pop[i].strategy);
+    }
+    assert(strategies.size() == pop.size() && "move data not correctly made");
+}
+
+Rcpp::DataFrame moveData::returnMoveData() {
+    return Rcpp::DataFrame::create(
+        Named("strategy") = strategies,
+        Named("moved") = moved
+    );
+}
 
 /// function to count strategy proportions
 std::vector<double> getPopStrategyProp (std::vector<Agent> &pop) {
@@ -42,29 +65,14 @@ std::vector<double> getPopStrategyProp (std::vector<Agent> &pop) {
   return vProp;
 }
 
-std::vector<double> getPopStrategyDistance(std::vector<Agent> &pop) {
-    std::vector<double> vMoved (5, 0.0);
-    std::vector<int> vCount (5, 0);
-    for(size_t i = 0; i < pop.size(); i++) {
-      vCount[pop[i].strategy]++;
-      vMoved[pop[i].strategy] += pop[i].moved;
-    }
-    for(size_t i= 0; i < vMoved.size(); i++) {
-        vMoved[i] = vMoved[i] / static_cast<double>(vCount[i]);
-    }
-    return vMoved;
-}
-
 void genData::updateGenData (std::vector<Agent> &pop, const int g) {
 
     std::vector<double> strategyProp = getPopStrategyProp(pop);
-    std::vector<double> strategyMoved = getPopStrategyDistance(pop);
     std::vector<int> stratvec = {0, 1, 2, 3, 4};
 
     gen.push_back(std::vector<int> (5, g));
     strategy.push_back(stratvec);
     prop.push_back(strategyProp);
-    dist_moved.push_back(strategyMoved);
 }
 
 void fitnessData::updateFitnessData(std::vector<Agent> &pop, const int g) {
@@ -84,8 +92,7 @@ Rcpp::List genData::returnGenData() {
         genDataList.push_back(Rcpp::DataFrame::create(
                     Named("gen") = gen[li],
                     Named("strategy") = strategy[li],
-                    Named("prop") = prop[li],
-                    Named("mean_distance") = dist_moved[li]));
+                    Named("prop") = prop[li]));
     }
 
     return genDataList;
