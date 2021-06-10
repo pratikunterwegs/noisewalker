072396c70ef1bd9a36a21817c145c46ced5a6250
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-10 23:26:45 +0200

    simplify sim

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,10 +1,10 @@
-#ifndef SIMULATION_H
-#define SIMULATION_H
 
 #include <iostream>
-#include "parameters.h"
-#include "agent.h"
-#include "noisewalker_tools.h"
+#include <chrono>
+#include "parameters.hpp"
+#include "agent.hpp"
+#include "datatypes.hpp"
+#include "simulation.hpp"
 #include "FastNoiseLite.h"
 #include <Rcpp.h>
 
@@ -12,38 +12,22 @@ using namespace Rcpp;
 
 /// function to evolve population
 Rcpp::List evolvePop(std::vector<Agent> &pop,
-               const int genmax, const int timesteps,
-               const float t_increment,
+               const int genmax,
+               const int timesteps,
                FastNoiseLite noise,
-               const int burnin)
+               const float perception)
 {
-    fitnessData thisFitnessData;
     genData thisGenData;
-    moveData thisMoveData;
-    float time = 0.f;
+    // loop through generations
     for (int gen = 0; gen < genmax; ++gen) {
         for (int t = 0; t < timesteps; ++t) {
-            time += t_increment;
-            // if gen has not changed then move and forage
-            popMoveForage(pop, noise, time);
+            popMoveForageCompete(pop, noise, perception, 8, 0.f); // set manually
         }
-        time = 0.f;
 
-        if (gen > burnin) {
-            thisGenData.updateGenData(pop, gen);
-            thisFitnessData.updateFitnessData(pop, gen);
-        }
-        if (gen == (genmax - 2))
-        {
-            thisMoveData.updateMoveData(pop);
-        }
+        // reproduce once generation is done
         doReproduce(pop);
     }
-    return Rcpp::List::create(
-        Named("pop_comp") = thisGenData.returnGenData(),
-        Named("fitness") = thisFitnessData.returnFitnessData(),
-        Named("movement") = thisMoveData.returnMoveData()
-    );
+    return thisGenData.returnGenData();
 }
 
 
@@ -53,42 +37,35 @@ Rcpp::List evolvePop(std::vector<Agent> &pop,
 //' arguments to the corresponding R function.
 //' 
 //' @param popsize The population size.
-//' @param percep_range The perception range.
 //' @param genmax The maximum number of generations per simulation.
 //' @param timesteps The number of timesteps per generation.
-//' @param t_increment Rate of landscape change.
+//' @param perception The perception range.
 //' @param nOctaves Number of octaves. May be thought of as small scale 
 //' variability. Must be an integer value between 1 and 8. Higher values
 //' result in landscapes with more small scale noise.
 //' @param frequency Frequency of noise. May be thought of as large scale
 //' variability. May be any double value between 1.0 and 16.0. Higher values
 //' mean more patchy landscapes.
-//' @param burnin Initial, unrecorded generations.
 //' @return A dataframe of evolved pop strategy count.
 // [[Rcpp::export]]
 Rcpp::List run_noisewalker(
         const int popsize,
-        const float percep_range,
-        const int genmax, const int timesteps,
-        const float t_increment,
-        const int nOctaves, const double frequency,
-        const int burnin) {
+        const int genmax, 
+        const int timesteps,
+        const float perception,
+        const int nOctaves, 
+        const double frequency) {
     
-    // set the random number generation etc
-    const gsl_rng_type * T;
-    gsl_rng_env_setup();
-    T = gsl_rng_default;
-    r = gsl_rng_alloc (T);
-    
+    // set up seed etc
     unsigned seed = static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count());
+    rng.seed(seed);
     
     // init pop
-    std::vector<Agent> pop (popsize, Agent(1, percep_range));
+    std::vector<Agent> pop (popsize);
     // random position
-    popRandomPos(pop, landsize);
+    popRandomPos(pop, landsize); // landsize is fixed in parameters.hpp
     // random weights
-    popRandomWeights(pop);
-    popRandomStrategy(pop);
+    popRandomTraits(pop);
     
     // make the ancestral landscape
     FastNoiseLite noise;
@@ -97,9 +74,8 @@ Rcpp::List run_noisewalker(
     noise.SetFractalOctaves(nOctaves);
     
     // do evolution
-    Rcpp::List thisData = evolvePop(pop, genmax, timesteps, t_increment, noise, burnin);
+    Rcpp::List thisData = evolvePop(pop, genmax, timesteps, noise, perception);
 
     return thisData;
 }
 
-#endif // SIMULATION_H
