ffe99ec76a8a2c4c3b770acc7b48f1949ed7d117
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-11 11:36:14 +0200

    remove old params

--- src/parameters.hpp
+++ src/parameters.hpp
@@ -2,19 +2,6 @@
 
 #include <random>
 
-// population params
-const int popSize = 1000; // not too many
-const int generations = 1000;
-const int timePerGen = 100;
-
-// costs
-const float costMove = 0.01f;
-const float costSensing = 0.01f;
-const float costCompete = 0.001f;
-
-// landscape params
-const float landsize = 100.f;
-
 // mutation params
 const float mutProb = 0.01f;
 const float mutSize = 0.001f;
