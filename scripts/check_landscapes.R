5995ef475ee47608fae796bade243d59f13f4d7f
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-12 11:37:10 +0200

    acf function

--- scripts/check_landscapes.R
+++ scripts/check_landscapes.R
@@ -7,6 +7,25 @@ library(data.table)
 # load the lib, better to restart R
 library(noisewalker)
 
+# autocorrelation structures functions
+# small function for acf
+get_acf_vals <- function(z, ...) {
+    acfout = stats::acf(z, ..., plot = FALSE)
+    data.frame(
+        acfval = acfout$acf,
+        acflag = acfout$lag
+    )
+}
+
+# small fun for acf range below 0.2
+get_acf_range <- function(z, corr_val = 0.1) {
+  acfr <- which(z["acfval"] < corr_val)[1]
+  if (is.na(acfr)){
+    acfr <- Inf
+  }
+  return(acfr)
+}
+
 # prepare combinations
 params = CJ(
     landsize = 5,
@@ -26,6 +45,12 @@ data = lapply(seq(nrow(params)),
                       increment = params[i]$increment,
                       clamp = params[i]$clamp
                   )
+                  
+                  # autocorrelation structure on a single row
+                  # this is very very crude but likely indicative
+                  acfstr = get_acf_vals(land[1, ])
+                  # add parameters
+
                   # make dataframe in the form x, y, value
                   land = as.data.table(land)
                   setnames(land, as.character(seq(ncol(land))))
@@ -34,18 +59,19 @@ data = lapply(seq(nrow(params)),
                               variable.name = "y")
                   land$y = as.numeric(land$y)
                   
-                  # return land
-                  land
+                  # return land and autocorrelation range
+                  list(land = land, acfstr = acfstr)
               })
 
 # combine and plot
 subplots = Map(function(df, p, l, i) {
-    ggplot(df)+
+    ggplot(df$land)+
         geom_tile(
             aes(x,y,fill = value)
         )+
         scale_fill_viridis_c(
-            option = "B"
+            option = "B",
+            limits = c(0, 1)
         )+
         coord_fixed()+
         theme_void()+
@@ -56,10 +82,48 @@ subplots = Map(function(df, p, l, i) {
         ))
 }, data, params$frequency, params$landsize, params$increment)
 
+# make acf plots
+subplots_acf = Map(function(df, p, l, i) {
+    ggplot(df$acfstr)+
+        geom_hline(
+            yintercept = c(0, 0.1),
+            lty = c(1, 2)
+        )+
+        geom_line(
+            aes(acflag, acfval),
+            col = "red"
+        )+
+        coord_cartesian(
+            ylim = c(-0.5, 1)
+        )+
+        theme_test()+
+        labs(title = glue::glue(
+            "frequency = {p}
+             landsize = {l}
+             increment = {i}"
+        ))
+}, data, params$frequency, params$landsize, params$increment)
+
 # wrap plots
-plot = wrap_plots(subplots, ncol = 3)
+plot_land = wrap_plots(subplots, ncol = length(subplots)) +
+    plot_layout(
+        guides = "collect"
+    ) &
+    theme(
+        legend.position = "bottom"
+    )
+plot_acf = wrap_plots(subplots_acf, ncol = length(subplots))
+
+# wrap all plots
+plots = wrap_plots(
+    plot_land,
+    plot_acf,
+    design = "A\nA\nA\nB",
+    ncol = 1
+)
 
 # save
 ggsave(
-    plot, filename = "figures/fig_landscape_options.png"
+    plots, filename = "figures/fig_landscape_options.png"
 )
+
