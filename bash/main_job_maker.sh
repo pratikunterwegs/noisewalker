0d39a5bc6de579b21ccc3fd611c2997fb625866f
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-12 22:13:55 +0200

    reduce mem use

--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
+#SBATCH --time=01:30:00
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=noisewalker_simulation
 #SBATCH --array=1-n_array
 #SBATCH --output=/home/p284074/noisewalker/data/output/noisewalker_output_%a.log
-#SBATCH --mem=5000
+#SBATCH --mem=3000
 
 pwd
 ml load R/4.0.0-foss-2020a
