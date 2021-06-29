a158f402951a04f9d9ea5ee881b3b971b28d75d1
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-29 09:30:06 +0200

    remove compete cost and add risk

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -31,11 +31,8 @@ getLandscape <- function(landsize, octaves, frequency, increment, clamp) {
 #' @param directions The number of points at which agents sense resources,
 #' at a fixed distance of \code{perception} units away from them.
 #' @param costMove The cost per move; distance moved is assumed constant.
-#' @param costCompete The cost per neighbour within the perception range.
-#' @param nOctaves Number of octaves. May be thought of as small scale 
-#' variability. Must be an integer value between 1 and 8. Higher values
-#' result in landscapes with more small scale noise.
-#' @param frequency Frequency of noise. May be thought of as large scale
+#' @param freqRisk The patchiness of the landscape of costs.
+#' @param freqRes Frequency of noise. May be thought of as large scale
 #' variability. May be any double value between 1.0 and 16.0. Higher values
 #' mean more patchy landscapes.
 #' @param landsize The size of the landscape after which movement is wrapped.
@@ -43,7 +40,7 @@ getLandscape <- function(landsize, octaves, frequency, increment, clamp) {
 #' @param clamp The threshold value of the landscape below which, the agents
 #' sense and receive zero resources. Needed because noise has values -1 to +1.
 #' @return A dataframe of evolved pop strategy count.
-run_noisewalker <- function(popsize, genmax, timesteps, perception, directions, costMove, costCompete, nOctaves, frequency, landsize, clamp) {
-    .Call(`_noisewalker_run_noisewalker`, popsize, genmax, timesteps, perception, directions, costMove, costCompete, nOctaves, frequency, landsize, clamp)
+run_noisewalker <- function(popsize, genmax, timesteps, perception, directions, costMove, freqRisk, freqRes, landsize, clamp) {
+    .Call(`_noisewalker_run_noisewalker`, popsize, genmax, timesteps, perception, directions, costMove, freqRisk, freqRes, landsize, clamp)
 }
 
