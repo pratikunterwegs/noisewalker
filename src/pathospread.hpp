ef18c78ed1e1be5f5721d4b074afe995762e3256
Pratik Gupte <pratikgupte16@gmail.com>
2021-07-11 11:42:46 +0200

    declare spread funs

---
+++ src/pathospread.hpp
@@ -0,0 +1,24 @@
+/// functions to infect an agent and spread disease
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <algorithm> 
+#include <cassert>
+
+#include <Rcpp.h>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/index/rtree.hpp>
+
+#include "agent.hpp"
+
+/// function to infect n individual
+void popIntroPathogen(std::vector<Agent> &pop, const int nInfected);
+/// function to spread pathogen
+void popPathogenSpread(std::vector<Agent> &pop, const float perception,
+                       const float pTransmit);
+/// function for pathogen cost
+void popPathogenCost(std::vector<Agent> &pop, const float costInfection);
