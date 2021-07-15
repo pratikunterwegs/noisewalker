6c2905ecb62021952fc5e38f6c6696a4858d73dc
Pratik Gupte <pratikgupte16@gmail.com>
2021-07-15 16:15:50 +0200

    update run sim cluster

--- scripts/run_sim_cluster.R
+++ scripts/run_sim_cluster.R
@@ -26,11 +26,14 @@ data_evolved_pop = run_noisewalker(
     perception = params$perception[row_n],    
     directions = params$directions[row_n],  
     costMove = params$costMove[row_n],
-    costCompete = params$costCompete[row_n],
-    nOctaves = params$nOctaves[row_n],   
-    frequency = params$frequency[row_n],  
+    freqRes = params$freqRes[row_n],
     landsize = params$landsize[row_n],   
-    clamp = params$clamp[row_n]
+    clamp = params$clamp[row_n],
+    random_traits = params$random_traits[row_n],
+    allow_compete = params$allow_compete[rown],
+    scenario = params$scenario[row_n],
+    pTransmit = params$scenario[pTransmit],
+    costInfection = params$costInfection[row_n]
 )
 
 # add generation to dataframe
@@ -49,17 +52,9 @@ data_evolved_pop = append(
   these_params
 )
 
-# name of rdata file
-output_file = Reduce(
-  paste,
-  mapply(
-    function(p, np) {
-      paste(np, p, sep = "_")
-    }, these_params, names(these_params)
-  )
-)
+# name of rdata file and export
 output_file = glue::glue(
-  'data/output/{output_file}.Rdata'
+  'data/output/output_file_{row_n}.Rds'
 )
 
 # save
