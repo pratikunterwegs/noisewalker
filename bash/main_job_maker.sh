4c4174bb49b0b65f8c540c21a3e45b8c06351d63
Pratik Gupte <pratikgupte16@gmail.com>
2021-07-19 15:36:17 +0200

    less time

--- bash/main_job_maker.sh
+++ bash/main_job_maker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=010:30:00
+#SBATCH --time=02:30:00
 #SBATCH --partition=regular
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=noisewalker_simulation
 #SBATCH --array=1-n_array
 #SBATCH --output=/home/p284074/noisewalker/data/output/noisewalker_output_%a.log
-#SBATCH --mem=2000
+#SBATCH --mem=1000
 
 pwd
 ml load R/4.0.0-foss-2020a
