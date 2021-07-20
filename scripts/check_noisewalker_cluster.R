24061bc4a8a54c4c0a693a37f9d9787101b4ddb1
Pratik Gupte <pratikgupte16@gmail.com>
2021-07-20 12:33:32 +0200

    increase cost disease

--- scripts/check_noisewalker_cluster.R
+++ scripts/check_noisewalker_cluster.R
@@ -3,10 +3,10 @@ library(noisewalker)
 password = readLines("data/password")
 
 # check cluster
-# noisewalker::check_prepare_cluster(
-#   ssh_con = "p284074@peregrine.hpc.rug.nl",
-#   password = password
-# )
+noisewalker::check_prepare_cluster(
+  ssh_con = "p284074@peregrine.hpc.rug.nl",
+  password = password
+)
 
 # prepare parameters
 noisewalker::make_parameter_file(
@@ -17,14 +17,14 @@ noisewalker::make_parameter_file(
     perception = 0.1,
     directions = 4,
     costMove = 0.001,
-    freqRes = c(0.1, 0.5, 1),
+    freqRes = c(0.5, 1),
     landsize = 10,
     clamp = 0.0,
     random_traits = F,
     allow_compete = c(T),
     scenario = c(0, 1, 2),
-    pTransmit = c(0.1, 0.5, 1),
-    costInfection = 0.01,
+    pTransmit = c(0.5, 1),
+    costInfection = c(0.1, 0.5),
     which_file = "data/parameters/parameters_full.csv"
 )
 
