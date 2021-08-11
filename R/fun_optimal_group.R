125c26ea329cbdf0bf2f21e60a6ea84cb8bc2926
Pratik Gupte <pratikgupte16@gmail.com>
2021-08-11 13:44:40 +0200

    update docu

--- R/fun_optimal_group.R
+++ R/fun_optimal_group.R
@@ -1,16 +1,35 @@
 #' Get optimal group sizes.
 #'
-#' @param data The trait dataset
 #' @param coef_agents The agent preference.
 #' @param coef_agents_sq The agent count squared preference.
 #' @return A vector of optimal group sizes
 #' @export
 #'
-optimal_group_size = function(data, 
-    coef_agents, coef_agents_sq) {
+optimal_group_size = function(coef_agents, coef_agents_sq) {
   
   # formula for the X-coord of the axis of symmetry of a parabola
   # -b/(2a)
-  - data[[coef_agents_sq]] / (2 * data[[coef_agents]])
+  - coef_agents_sq / (2 * coef_agents)
 
+}
+
+#' Get response to agents.
+#' 
+#' @param coef_agents The agent preference.
+#' @param coef_agents_sq The agent count squared preference.
+#' @param plot Whether to plot.
+#' @return A vector of group size preferences.
+#' @export
+#' 
+agent_response = function(coef_agents, coef_agents_sq, plot = FALSE) {
+
+  resp = (coef_agents * (seq(from = 0, to = 20, by = 5))) + 
+    (coef_agents_sq * (seq(from = 0, to = 20, by = 5))^2)
+
+  if(plot) {
+    plot(resp, type = "b", xlab = "group size", ylab = "pref", pch = 16,
+         main = sprintf("coef_agent = %.4f coef_ag_sq = %.4f", coef_agents, coef_agents_sq))
+  } else {
+     resp
+  }
 }
--- scripts/check_noisewalker.R
+++ scripts/check_noisewalker.R
@@ -6,8 +6,9 @@ library(data.table)
 
 # libraries to build and install
 Rcpp::compileAttributes()
+Sys.setenv("LIB_GSL" = "C:/local323")
 build()
-sink("install_output.log"); install(); sink()
+sink("install_output.log"); install(quick = T, upgrade = "never"); sink()
 document()
 
 # load the lib, better to restart R
@@ -16,46 +17,22 @@ library(noisewalker)
 
 # test run
 a = noisewalker::run_noisewalker(
-    popsize = 100, 
-    genmax = 50, 
+    popsize = 500, 
+    genmax = 100, 
     timesteps = 25, 
     perception = 0.1,
     directions = 4,
-    costMove = 0.0,
+    costMove = 0.01,
     freqRes = 1,
-    landsize = 5,
+    landsize = 10,
     clamp = 0.0,
-    random_traits = F,
+    random_traits = T,
     allow_compete = T,
     scenario = 1,
     pTransmit = 0.5,
-    costInfection = 0.01
+    costInfection = 0.01,
+    recordPos = F
 )
-
-data = a[["posdata"]]
-
-posdata = Map(function(df, g) {
-    df$gen = g
-    df
-}, data[["pos_data"]], data[["gens"]])
-
-# bind
-posdata = rbindlist(posdata)
-
-# plot
-ggplot(posdata[gen %% 10 == 0,])+
-    geom_bin2d(
-        aes(x, y)
-    )+
-    scale_fill_viridis_c(
-        option = "F", direction = -1,
-        trans = "log10"
-    )+
-    facet_wrap(~gen)+
-    coord_fixed()
-
-# save data
-save(a, file = "data/output/test_data.Rds")
 
 # get data
 data = handle_rcpp_out(a[["gendata"]])
@@ -64,7 +41,7 @@ data = handle_rcpp_out(a[["gendata"]])
 ggplot(data)+
     geom_bin2d(
         aes(gen, energy),
-        binwidth = c(1, 0.1)
+        binwidth = c(1, 0.5)
     )+
     # geom_point(
     #     aes(gen, energy),
@@ -76,9 +53,27 @@ ggplot(data)+
         direction = -1
     )+
     coord_cartesian(
-        # ylim = c(0, 10)
+        # xlim = c(0, 100)
     )
 
+# get last gen pop
+data_last = data[gen %in% c(max(gen), min(gen)),]
+data_last[, resp := Map(agent_response, coef_nbrs, coef_risk)]
+data_last[, id := rep(seq(nrow(data_last) / 2), 2)]
+
+data_summary = data_last[, list(
+    resp = unlist(resp, recursive = F),
+    grpsize = seq(from = 0, to = 20, by = 5)
+), by = c("id", "gen")]
+
+ggplot(data_summary)+
+    geom_path(
+        aes(grpsize, resp, 
+            group = interaction(id, gen), 
+            colour = factor(gen)),
+        alpha = 0.2
+    )+
+    coord_cartesian(expand = F)
 
 # tanh transform
 data[, c("coef_food", "coef_nbrs", "coef_risk") := lapply(
@@ -100,8 +95,8 @@ ggplot(data_summary)+
         aes(gen, value, fill = N)
     )+
     scale_fill_viridis_c(
-        option = "C",
-        begin = 0, end = 0.95,
+        option = "A",
+        # begin = 0, end = 0.95,
         direction = -1
     )+
     # coord_cartesian(xlim = c(0, 500))+
